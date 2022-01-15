c791219b4ae99344bb03fa03a40315fd26adb0f3
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-01-15 22:56:39 +0300

    added hover effect on pizza icon description

--- script.js
+++ script.js
@@ -1,24 +1,74 @@
 $(document).ready(function(){
-   
     var elementNumbers = ["1", "2", "3", "4", "5", "6", "7", "8"]
+  
     elementNumbers.forEach(function(element) {
-        $("#content"+ element).click(function() {
-            $(this).css('opacity','0.7') 
+        $("#content"+ element).hover(function() {
+          $("#pizza" + element).show();},
+
+         
+
             
-            $('#pizza' + element ).show()
-            
+         
+         function() {
+            $("#pizza" + element).hide()
+          
+         })
+
+        $('#hidden' + element).click(function(){
+            $(this).hide()
+            $('#whatWeDo' + element)
+      
         })
+    });
+})
+    // var elementNumbers = ["1", "2", "3", "4", "5", "6", "7", "8"]
+  
+    //   elementNumbers.forEach(function(element) {
+    //       $("#content"+ element).hover(function() {
+    //         //   $(this).hide()
+    //           $('#project' + element ).show()
+              
+    //       })
+  
+    //       $('#hidden' + element).click(function(){
+    //           $(this).hide()
+    //           $('#whatWeDo' + element)
+        
+    //         });
+    //   })
+    // })
+    //   elementNumbers.forEach(function(element) {
+    //     $("#content"+ element).click(function() {
+    //         $(this).css('opacity','0')
+    //         alert("you clicked me"+ element)
+            
+    //         $('#project' + element ).show()
+            
+    //     })
 
     
             
         
 
-        $('#project' + element).click(function(){
-            $('#project' + element ).css('display','none')
-            $('#content' + element).css('opacity', '1')
+    //     $('#project' + element).click(function(){
+    //         $('#project' + element ).css('display','none')
+    //         $('#content' + element).css('opacity', '1')
             
       
-          });
-    })
+    //       });
+    // })
 
-})
+    
+
+    // document.getElementById("formMessage").addEventListener("submit", validateInputHere)
+    // function validateInputHere (){
+            
+    //     var username= document.getElementById('username').value;
+    //     var useremail= document.getElementById('useremail').value;
+             
+    //     alert("hello, " + username + " we have received your message. Thank you for reaching out to us.")
+    // }
+
+
+   
+// })
