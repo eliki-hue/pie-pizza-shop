7e5ec880a5376fdfd047b379c92515192ad61def
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-01-19 08:00:51 +0300

    added pizza crust objects in Pizzacrust constructor

--- script.js
+++ script.js
@@ -10,12 +10,24 @@ const cartContent = document.querySelector(".cart-content");
 const productsDOM = document.querySelector(".product-center");
 
 // creating the constructors pizzaCrust % Topping
-function PizzaCrust(name, price, quantity, id){
+function PizzaCrust(name, price, id, img){
     this.name =name;
     this.price = price;
-    this.quantity = quantity
+    
     this.id = id
+    this.img = img
 }
+
+const cracker = new PizzaCrust('Cracker',1000,'1','images/Cracker-Pizza.jpg' )
+const cheese = new PizzaCrust('cheese',2000,'2','images/cheese-crust.jpg')
+const double = new PizzaCrust('Double crust',3000, '3', 'images/double-crust.jpg')
+const flat = new PizzaCrust('Flat bed', 4000, '4','images/flat-bread.jpg')
+const gluten = new PizzaCrust('Gluten free',5000,'5','images/gluten-free-pizza-crust.jpg')
+const stuffed = new PizzaCrust('Stuffed',6000, '6', 'images/stuffed pizza crust.jpg')
+const thin =new PizzaCrust('Thin & cripsy', 7000, '7', 'images/Thin & crispy pizza crust.jpg')
+const wrapping = new PizzaCrust('Wrapping', 8000, '8', 'images/wrapping-pizza-crust.jpeg')
+
+
 function Topping(name, cost, id){
     this.name = name;
     this.cost = cost;
@@ -23,6 +35,20 @@ function Topping(name, cost, id){
 }
 //  creating prototype
 PizzaCrust.prototype.size = ()
+
+let cart = []
+
+// displaying the items
+function Display{
+    
+}
+
+// eventLISTER
+
+document.addEventListener("DOMContentLoaded",()=> {
+    const display = new Display();
+})
+
 
 
 
